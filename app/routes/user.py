e9a0d1338a119e85de25b950ad9e70de12463102
Jhonatabz <jhonatabezerra028@gmail.com>
2025-06-19 14:33:27 -0300

    fix: lógioca de inserção de usuários

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from app.models.user import User, UserPublic, UserDB
+from app.models.user import User, UserVerify
 import app.db.connection as database
 
 templates = Jinja2Templates(directory='app/templates/user')
@@ -17,8 +17,16 @@ async def cadastrar_form(request: Request):
     return templates.TemplateResponse("cadastro.html", {"request": request})
 
 @user_app.post("/usuario/login")
-async def login_user(request: Request):
-    return templates.TemplateResponse("login_response.html", {"request": request})
+async def login_user(request: Request,
+    email: str = Form(...),
+    senha: str = Form(...)
+):
+    user = UserVerify(email=email, senha=senha)
+    verificacao = database.verificar_login(**user.model_dump())
+    if verificacao:
+        return templates.TemplateResponse("login_response.html", {"request": request})
+    else: 
+        return templates.TemplateResponse("cadastro_error.html", {"request": request, "email": email})
 
 @user_app.post('/usuario/cadastrar', response_class=HTMLResponse, status_code=200)
 async def create_user(request: Request,
@@ -26,8 +34,9 @@ async def create_user(request: Request,
     email: str = Form(...),
     senha: str = Form(...)
 ):
-    inserir = database.inserir_usuario(nome, email, senha)
+    user = User(nome=nome, email=email, senha=senha)
+    inserir = database.inserir_usuario(**user.model_dump())
     if inserir:
-        return templates.TemplateResponse("cadastro_error.html", {"request": request, "email": email})
-    else:
         return templates.TemplateResponse("cadastro_response.html", {"request": request, "user": {"nome": nome, "email": email}})
+    else:
+        return templates.TemplateResponse("cadastro_error.html", {"request": request, "email": email})
